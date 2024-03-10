72289b90872f68d17bbd7c27ed41fa8e0a20bda5
akshatg5 <akshatgirdhar5@gmail.com>
2024-03-11 01:20:28 +0530

    Landing Page

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { DisplayCard } from "./LandingPageDisplayCard";
 
 export const LandingPage = () => {
-    return (
-        <div>
-        <div className="flex" >
-            <Link to="/youtubeClone" >
-                <img src="/youtube-logo.jpg" className="w-20 h-20" />
-                <button>Youtube Clone</button>
-            </Link>
-            <Link to="/dukaanAssignment" >
-                <button>Dukaan Assignment</button>
-            </Link>
-        </div>
-        </div>
-    )
-}
+  return (
+    <div className="bg-black h-screen w-screen justify-center">
+      <div className="flex justify-center items-center align-middle">
+        <DisplayCard
+          link={"/youtubeClone"}
+          displayImg={"/youtube-logo.jpg"}
+          projectTitle={"Youtube Clone"}
+          projectDescription={
+            "Youtube Clone made using ReactJS and Tailwind CSS"
+          }
+          githubLink={
+            "https://github.com/akshatg5/Frontend-projects/tree/main/src/components/Youtube-components"
+          }
+        />
+        <DisplayCard
+          link={"/dukaanAssignment"}
+          displayImg={"/dukaanImg.png"}
+          projectTitle={"Dukaan Assignment"}
+          projectDescription={
+            "Dukaan Assignment given by Dukaan App : https://twitter.com/mydukaanapp. Made using ReactJS and Tailwind "
+          }
+          githubLink={
+            "https://github.com/akshatg5/Frontend-projects/tree/main/src/components/Dukaan-components"
+          }
+        />
+      </div>
+    </div>
+  );
+};
