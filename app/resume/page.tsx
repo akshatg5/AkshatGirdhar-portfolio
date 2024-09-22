99aa7d1cbd90e7089653e5786505bd3272295995
Akshat Girdhar <131449376+akshatg5@users.noreply.github.com>
2024-09-22 18:59:48 +0530

    Update page.tsx

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -13,13 +13,6 @@ export default function Resume() {
             </iframe>
         </div>
         <div>
-        <a
-          href="/path-to-your-resume.pdf"
-          download
-          className="mt-8 bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-          Download Resume
-        </a>
           </div>
       </div>
     )
